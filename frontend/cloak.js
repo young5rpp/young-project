3a459220c48bd33da956cc1afa1e9d1b343748c7
young5rpp <youngta5v@gmail.com>
2025-04-23 10:51:48 -0400

    Update cloak.js

--- frontend/cloak.js
+++ frontend/cloak.js
@@ -1,1 +1,13 @@
+function cloakAsGoogleDocs() {
+  document.title = "Untitled document - Google Docs";
+  const link = document.querySelector("link[rel~='icon']");
+  if (link) {
+    link.href = "https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x32.png";
+  } else {
+    const newLink = document.createElement("link");
+    newLink.rel = "icon";
+    newLink.href = "https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x32.png";
+    document.head.appendChild(newLink);
+  }
+}
 
